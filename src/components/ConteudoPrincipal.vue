435ab37e1c9e767e32a0a4364a263f6302b1a770
Caue Gomes de Oliveira <cauego96@hotmail.com>
2024-07-26 20:33:55 -0300

    Adjust tag status

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -20,7 +20,7 @@ export default {
             </span>
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
                 <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente"> 
-                    <Tag :texto="ingrediente"/>
+                    <Tag :texto="ingrediente" ativa/>
                 </li>
             </ul>
             <p v-else class="paragrafo lista-vazia">
