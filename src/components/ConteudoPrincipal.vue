f69e18ce0e9e214cda91024271b7b6a25a49255e
Caue Gomes de Oliveira <cauego96@hotmail.com>
2024-07-26 20:22:37 -0300

    Reusing Css

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import Tag from './Tag.vue';
 
 export default {
     data() {
@@ -7,7 +8,7 @@ export default {
             ingredientes: ['Alho', 'Manteiga', 'Oregano']
         }
     },
-    components: { SelecionarIngredientes }
+    components: { SelecionarIngredientes, Tag }
 }
 </script>
 
@@ -18,8 +19,8 @@ export default {
                 Sua lista:    
             </span>
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente" class="ingrediente"> 
-                    {{ ingrediente }}
+                <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente"> 
+                    <Tag :texto="ingrediente"/>
                 </li>
             </ul>
             <p v-else class="paragrafo lista-vazia">
@@ -57,18 +58,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-    transition: 0.2s;
-    color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
 }
 
 .lista-vazia {
