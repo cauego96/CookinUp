d6c355210f7abeeace00484122062584fe4188b6
Caue Gomes de Oliveira <cauego96@hotmail.com>
2024-07-27 09:09:11 -0300

    Static page two

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -35,7 +35,10 @@ export default {
           @remover-ingrediente="removerIngrediente"
           @buscar-receitas="navegar('MostrarReceitas') "
         />
-        <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"/>
+        <MostrarReceitas 
+          v-else-if="conteudo === 'MostrarReceitas'"
+          @editar-receitas="navegar('SelecionarIngredientes')"
+        />
     </main>
 </template>
 
